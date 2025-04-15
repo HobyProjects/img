522d9af1f0c6b7524ce75e68326f0bd242e068d0
HobyProjects <hobbyprojects.dev@gmail.com>
2025-04-15 13:52:15 +0530

    Update version to 1.0.6; enhance version logging and add MIT License

--- img.cpp
+++ img.cpp
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2025 HobyProjects
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
 #include "img_pch.hpp"
 
 /**
@@ -34,12 +58,24 @@ class bit_reader {
             static_assert(std::is_integral_v<T>, "T must be an integral type");
             T result = 0;
             for (size_t i = 0; i < sizeof(T); ++i) {
-                result |= (m_data[m_bit_offset >> 3] >> (m_bit_offset & 7)) << (i * 8);
-                ++m_bit_offset;
+                result = (result << 8) | m_data[m_bit_offset >> 3];
+                m_bit_offset += 8;
             }
             return result;
-        }
+        }  
 
+        /**
+         * @brief Reads a byte array of size bytes from the bit stream.
+         * @details
+         * This function reads size bytes from the bit stream and returns
+         * a pointer to the byte array. The least significant bit of the
+         * first byte is considered the most significant bit of the first
+         * byte in the byte array, and the most significant bit of the
+         * last byte is considered the least significant bit of the last
+         * byte in the byte array.
+         * @param size The size of the byte array to read.
+         * @return A pointer to the byte array read from the bit stream.
+         */
         uint8_t* read(uint32_t size) noexcept {
             if (m_bit_offset + size > m_size * 8) {
                 IMG_DEBUG_LOG("An Attempt to read beyond buffer size\n");
@@ -117,6 +153,37 @@ static uint32_t unique_xid() noexcept {
     return ++s_xid;             // Increment and return the unique ID
 }
 
+/**
+ * @struct img_rgb
+ * @brief This structure represents an RGB color.
+ * It contains the red, green, and blue channel values.
+ */
+struct img_rgb{
+    uint8_t r{0}; // Red channel value
+    uint8_t g{0}; // Green channel value
+    uint8_t b{0}; // Blue channel value
+
+    img_rgb() = default; // Default constructor
+    img_rgb(uint8_t red, uint8_t green, uint8_t blue) : r(red), g(green), b(blue) {} 
+    ~img_rgb() = default; // Default destructor
+};
+
+/**
+ * @struct img_rgba
+ * @brief This structure represents an RGBA color.
+ * It contains the red, green, blue, and alpha channel values.
+ */
+struct img_rgba{
+    uint8_t r{0}; // Red channel value
+    uint8_t g{0}; // Green channel value
+    uint8_t b{0}; // Blue channel value
+    uint8_t a{0}; // Alpha channel value
+
+    img_rgba() = default; // Default constructor
+    img_rgba(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha) : r(red), g(green), b(blue), a(alpha) {} 
+    ~img_rgba() = default; // Default destructor
+};
+
 /******************************************************************
  *                         PNG IMAGE FILES                        *
  * ****************************************************************/
@@ -141,6 +208,48 @@ static constexpr uint32_t s_IMG_PNG_hIST_CHUNK = 0x68495354; // hIST chunk type
 static constexpr uint32_t s_IMG_PNG_tIME_CHUNK = 0x74494D45; // tIME chunk type
 static constexpr uint32_t s_IMG_PNG_pCAL_CHUNK = 0x7043414C; // pCAL chunk type
 static constexpr uint32_t s_IMG_PNG_sCAL_CHUNK = 0x7343414C; // sCAL chunk type
+
+static constexpr uint32_t s_CRC32_POLYNOMIAL = 0xEDB88320; // CRC32 polynomial
+static constexpr uint32_t s_CRC_TABLE_SIZE = 256;          // Size of the CRC table
+
+static uint32_t s_crc_table[s_CRC_TABLE_SIZE];             // CRC table
+static std::once_flag s_crc_table_init_flag;               // Flag to ensure CRC table is initialized only once
+
+/**
+ * @brief Generates the CRC table for CRC32 calculation.
+ * @details This function generates the CRC table used for CRC32 calculation.
+ * The table is generated using the polynomial defined in s_CRC32_POLYNOMIAL.
+ */
+static void img_png_generate_crc_table() noexcept {
+    for (uint32_t i = 0; i < s_CRC_TABLE_SIZE; ++i) {
+        uint32_t crc = i;
+        for (uint32_t j = 0; j < 8; ++j) {
+            if (crc & 1) {
+                crc = (crc >> 1) ^ s_CRC32_POLYNOMIAL;
+            } else {
+                crc >>= 1;
+            }
+        }
+        s_crc_table[i] = crc;
+    }
+}
+
+/**
+ * @brief Calculates the CRC32 checksum for a given data buffer.
+ * @details This function calculates the CRC32 checksum for a given data buffer
+ * using the CRC table generated by img_png_generate_crc_table().
+ * @param data Pointer to the data buffer.
+ * @param length Length of the data buffer in bytes.
+ * @return The calculated CRC32 checksum.
+ */
+static uint32_t img_png_crc32(const uint8_t* data, size_t length) noexcept {
+    uint32_t crc = 0xFFFFFFFF; // Initial CRC value
+    for (size_t i = 0; i < length; ++i) {
+        uint8_t byte = data[i];
+        crc = (crc >> 8) ^ s_crc_table[(crc ^ byte) & 0xFF]; // Update CRC value using the table
+    }
+    return ~crc; // Return the final CRC value
+}
 
 /**
  * @struct img_png_chunk
@@ -185,7 +294,6 @@ struct img_png_ihdr_chunck : img_png_chunk {
     uint8_t compression_method{0};              // Compression method of the image
     uint8_t filter_method{0};                   // Filter method of the image
     uint8_t interlace_method{0};                // Interlace method of the image
-    img_png_chunk* parent_chunck{nullptr};      // Pointer to the parent chunk (IHDR chunk)
 
     img_png_ihdr_chunck() = default;
     virtual ~img_png_ihdr_chunck() = default;
@@ -199,7 +307,6 @@ struct img_png_ihdr_chunck : img_png_chunk {
 struct img_png_idat_chunk : img_png_chunk {
     uint8_t* data{nullptr};                       // Pointer to the IDAT chunk data
     uint32_t length{0};                           // Length of the IDAT chunk data
-    img_png_chunk* parent_chunck{nullptr};        // Pointer to the IDAT chunk
 
     img_png_idat_chunk() = default;
     virtual ~img_png_idat_chunk() {
@@ -207,43 +314,135 @@ struct img_png_idat_chunk : img_png_chunk {
     }
 };
 
+/**
+ * @struct img_png_plte_chunk
+ * @brief This structure represents the PLTE chunk of a PNG image.
+ * It contains a vector of RGB colors and the length of the PLTE chunk data.
+ */
+struct img_png_plte_chunk : img_png_chunk {
+    std::vector<img_rgb> palette; // Vector to hold the palette colors
+    uint32_t length{0};           // Length of the PLTE chunk data
+
+    img_png_plte_chunk() = default;
+    virtual ~img_png_plte_chunk() = default; 
+};
+
+/**
+ * @struct img_png_tRNS_chunk
+ * @brief This structure represents the tRNS chunk of a PNG image.
+ * It contains a vector of alpha values and the length of the tRNS chunk data.
+ */
+struct img_png_tRNS_chunk : img_png_chunk {
+    std::vector<uint8_t> alpha; // Vector to hold the alpha values
+    uint32_t length{0};         // Length of the tRNS chunk data
+
+    img_png_tRNS_chunk() = default;
+    virtual ~img_png_tRNS_chunk() = default;
+};
+
+/**
+ * @brief Reads a PNG file and extracts its chunks.
+ * @details This function reads a PNG file and extracts its chunks. It verifies
+ * the PNG signature, calculates the CRC for each chunk, and stores the chunks
+ * in a vector. It also handles the IHDR, IDAT, and PLTE chunks specifically.
+ * @param spec Pointer to the image specification structure.
+ * @param data Pointer to the PNG file data.
+ * @param size Size of the PNG file data in bytes.
+ * @return True if the PNG file was read successfully, false otherwise.
+ */
 static bool img_png_read(const std::shared_ptr<img::image_specification>& spec, uint8_t* data, size_t size) noexcept {
-    bit_reader reader(data, size);                  // Create a bit reader for the data
-    uint64_t signature = reader.read<uint64_t>();   // Read the PNG signature
+    bit_reader bit(data, size);                  // Create a bit bit for the data
+    uint64_t signature = bit.read<uint64_t>();   // Read the PNG signature
     if(signature != s_IMG_PNG_SIGNATURE){
-        IMG_DEBUG_LOG("Invalid PNG signature: %llx\nFile:%s is not a PNG file", signature, spec->filename.c_str());
+        IMG_DEBUG_LOG("Invalid PNG signature: %llx, File:%s is not a PNG file\n", signature, spec->filename.c_str());
         return false;
     }
 
+    std::call_once(s_crc_table_init_flag, img_png_generate_crc_table); // Generate the CRC table
     std::vector<img_png_type_chunk_map> png_chunks; // Vector to hold PNG chunks 
 
     while(true){
         img_png_chunk chunk;                           
-        chunk.length = reader.read<uint32_t>();        
-        chunk.type = reader.read<uint32_t>();          
+        chunk.length = bit.read<uint32_t>();        
+        chunk.type = bit.read<uint32_t>();
+
+        if(chunk.length == 0){
+            IMG_DEBUG_LOG("Invalid chunk, length: %x, Possible corruption\n", chunk.length);
+            return false;
+        }
+        
+        uint8_t* chunk_data = bit.read(chunk.length);
+        if(chunk_data == nullptr){
+            IMG_DEBUG_LOG("Failed to read chunk data\n");
+            return false;
+        }
+
+        chunk.crc = bit.read<uint32_t>();                    // Read the CRC value for the chunk
+        size_t crc_input_size = 4 + chunk.length;
+        uint8_t* crc_input = new uint8_t[crc_input_size];
+
+        // Write type (big endian) first
+        crc_input[0] = (chunk.type >> 24) & 0xFF;
+        crc_input[1] = (chunk.type >> 16) & 0xFF;
+        crc_input[2] = (chunk.type >> 8) & 0xFF;
+        crc_input[3] = chunk.type & 0xFF;
+        memcpy(crc_input + 4, chunk_data, chunk.length);
+
+        // Now calculate CRC over type + data
+        uint32_t crc = img_png_crc32(crc_input, crc_input_size);
+        delete[] crc_input;
+
+        if(crc != chunk.crc){
+            IMG_DEBUG_LOG("Invalid CRC for chunk type: %x, expected: %x\n", chunk.type, crc);
+            delete[] chunk_data; // Free the chunk data
+            return false;
+        }
 
         if(chunk.type == s_IMG_PNG_IHDR_CHUNK){
             img_png_ihdr_chunck* ihdr = new img_png_ihdr_chunck();
-            ihdr->width = reader.read<uint32_t>();                  
-            ihdr->height = reader.read<uint32_t>();                 
-            ihdr->bit_depth = reader.read<uint8_t>();               
-            ihdr->color_type = reader.read<uint8_t>();              
-            ihdr->compression_method = reader.read<uint8_t>();      
-            ihdr->filter_method = reader.read<uint8_t>();           
-            ihdr->interlace_method = reader.read<uint8_t>();        
-            chunk.crc = reader.read<uint32_t>();                    
+            ihdr->width = (chunk_data[0] << 24) | (chunk_data[1] << 16) | (chunk_data[2] << 8) | chunk_data[3];
+            ihdr->height = (chunk_data[4] << 24) | (chunk_data[5] << 16) | (chunk_data[6] << 8) | chunk_data[7];
+            ihdr->bit_depth = chunk_data[8];
+            ihdr->color_type = chunk_data[9];
+            ihdr->compression_method = chunk_data[10];
+            ihdr->filter_method = chunk_data[11];
+            ihdr->interlace_method = chunk_data[12];                            
             png_chunks.emplace_back(img_png_type_chunk_map(chunk.type, ihdr)); 
         }
 
         if(chunk.type == s_IMG_PNG_IDAT_CHUNK){
             img_png_idat_chunk* idat = new img_png_idat_chunk();
             idat->length = chunk.length;                             
-            idat->data = reader.read(chunk.length);                  
-            chunk.crc = reader.read<uint32_t>();                     
+            idat->data = chunk_data;                                      
             png_chunks.emplace_back(img_png_type_chunk_map(chunk.type, idat));
         }
 
+        if(chunk.type == s_IMG_PNG_PLTE_CHUNK){
+            img_png_plte_chunk* plte = new img_png_plte_chunk();
+            plte->palette.reserve(chunk.length / 3); 
+            for (uint32_t i = 0; i < chunk.length; i += 3) {
+                plte->palette.emplace_back(img_rgb(chunk_data[i], chunk_data[i + 1], chunk_data[i + 2]));
+            }
+            plte->length = chunk.length;
+            png_chunks.emplace_back(img_png_type_chunk_map(chunk.type, plte)); 
+        }
+
+        if(chunk.type == s_IMG_PNG_tRNS_CHUNK){
+            img_png_tRNS_chunk* tRNS = new img_png_tRNS_chunk();
+            tRNS->alpha.reserve(chunk.length); 
+            for (uint32_t i = 0; i < chunk.length; ++i) {
+                tRNS->alpha.emplace_back(chunk_data[i]);
+            }
+            tRNS->length = chunk.length;
+            png_chunks.emplace_back(img_png_type_chunk_map(chunk.type, tRNS)); 
+        }
+
+        if(chunk.type != s_IMG_PNG_IDAT_CHUNK){
+            delete[] chunk_data; // Free the chunk data
+        }
+
         if(chunk.type == s_IMG_PNG_IEND_CHUNK){
+            png_chunks.emplace_back(img_png_type_chunk_map(chunk.type, nullptr));
             break;
         }
     }
